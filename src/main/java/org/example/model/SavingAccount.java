f6298786dc92a2292a472a5699760e4c68ede998
AKalyghnii <kalyghnii@gmail.com>
2026-09-04 15:28:09 +0500

    create accrue method and test

--- src/main/java/org/example/model/SavingAccount.java
+++ src/main/java/org/example/model/SavingAccount.java
@@ -1,46 +1,76 @@
 package org.example.model;
 
 import org.example.exceptions.BalanceLimitException;
+import org.example.util.Constant;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SavingAccount extends BankAccount {
     private int withdrawLimit;
+    private final int maxWithdrawalLimit;
+    private LocalDate dateLastAccrual = LocalDate.now();
 
     public SavingAccount(UUID userId) {
         super(userId);
-        this.withdrawLimit = 0;
+        this.withdrawLimit = Constant.MAX_WITHDRAWALS;
+        this.maxWithdrawalLimit = Constant.MAX_WITHDRAWALS;
     }
 
     public SavingAccount(UUID userId, int withdrawLimit) {
         super(userId);
         this.withdrawLimit = withdrawLimit;
+        this.maxWithdrawalLimit = withdrawLimit;
     }
 
     public SavingAccount(UUID userId, BigDecimal balance) {
         super(userId, balance);
-        this.withdrawLimit = 0;
+        this.withdrawLimit = Constant.MAX_WITHDRAWALS;
+        this.maxWithdrawalLimit = Constant.MAX_WITHDRAWALS;
     }
 
     public SavingAccount(UUID userId, BigDecimal balance, int withdrawLimit) {
         super(userId, balance);
         this.withdrawLimit = withdrawLimit;
+        this.maxWithdrawalLimit = withdrawLimit;
     }
 
     public SavingAccount(UUID id, UUID userId, BigDecimal balance) {
         super(id, userId, balance);
-        this.withdrawLimit = 0;
+        this.withdrawLimit = Constant.MAX_WITHDRAWALS;
+        this.maxWithdrawalLimit = Constant.MAX_WITHDRAWALS;
     }
 
     public SavingAccount(UUID id, UUID userId, BigDecimal balance, int withdrawLimit) {
         super(id, userId, balance);
         this.withdrawLimit = withdrawLimit;
+        this.maxWithdrawalLimit = withdrawLimit;
+    }
+
+    public SavingAccount(UUID id, UUID userId, BigDecimal balance, int withdrawLimit, LocalDate dateLastAccrual) {
+        this(id, userId, balance, withdrawLimit);
+        this.dateLastAccrual = dateLastAccrual;
     }
 
     public int getWithdrawLimit() {
         return withdrawLimit;
+    }
+
+    public void accrueInterestIfDue(Clock clock) {
+        LocalDate timeUpdated = LocalDate.now(clock);
+        int differenceMonth = (int) ChronoUnit.DAYS.between(dateLastAccrual, timeUpdated) / 30;
+        for (int i = 0; i < differenceMonth; i++) {
+            BigDecimal percent = this.balance.multiply(Constant.DEPOSIT_RATE);
+            this.balance = this.balance.add(percent);
+            this.withdrawLimit = this.maxWithdrawalLimit;
+        }
+
+        this.dateLastAccrual = this.dateLastAccrual.plusDays(differenceMonth * 30);
     }
 
     @Override
---
+++ src/main/java/org/example/util/Constant.java
@@ -0,0 +1,9 @@
+package org.example.util;
+
+import java.math.BigDecimal;
+
+
+public class Constant {
+    public final static BigDecimal DEPOSIT_RATE = new BigDecimal("0.16");
+    public final static int MAX_WITHDRAWALS = 5;
+}
